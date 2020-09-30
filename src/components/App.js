88ad097a63cd18910e46ff3fe9805f57e3bbec8a
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-29 22:57:51 -0700

    connected songDetail to app

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from "react";
 //import {selectSong} from '../actions'
 
 import "./App.css";
+import SongDetail from "./SongDetail";
 import SongList from "./SongList";
 
 function App() {
 	return (
-		<div className="App">
-			<SongList />
+		<div className="ui container grid">
+			<div className="ui row">
+				<div className="column eight wide">
+					<SongList />
+				</div>
+				<div className="column eight wide">
+					<SongDetail />
+				</div>
+			</div>
 		</div>
 	);
 }
